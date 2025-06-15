cb4e0f38c501001859f0b71345fc52b81ba4159e
JasonCai0529 <caiyl0529@gmail.com>
2025-06-14 23:02:32 -0500

    cleanUp&Comment

--- mp_mosaics/src/maptiles.cpp
+++ mp_mosaics/src/maptiles.cpp
@@ -74,38 +74,19 @@ Point<3> convertToXYZ(LUVAPixel pixel) {
 MosaicCanvas* mapTiles(SourceImage const& theSource,
                        vector<TileImage>& theTiles)
 {
-    std::cerr << "[mapTiles] entering mapTiles()" << std::endl;
 
     int rows = theSource.getRows();
     int cols = theSource.getColumns();
 
-    std::cerr << "[mapTiles] Source image is " << rows << " x " << cols << std::endl;
 
     MosaicCanvas* mosaic = new MosaicCanvas(rows, cols);
 
-    std::cerr << "[mapTiles] Allocated MosaicCanvas" << std::endl;
 
+    // each point corresponds to a TileImage
     vector<Point<3>> tile_point(theTiles.size());
-    // std::map<Point<3>, TileImage> tile_map;
-
-    // std::cerr << "[mapTiles] Starting tile preprocessing loop..." << std::endl;
-
-    // for (unsigned int i = 0; i < theTiles.size(); i++) {
-    //     std::cerr << "  [tile loop] i = " << i << std::endl;
-
-    //     LUVAPixel cur_pixel = theTiles[i].getAverageColor();
-    //     std::cerr << "    got average color: "
-    //               << "(" << cur_pixel.l << "," << cur_pixel.u << "," << cur_pixel.v << ")"
-    //               << std::endl;
-
-    //     Point<3> pt = convertToXYZ(cur_pixel);
-    //     tile_point[i] = pt;
-
-    //     std::cerr << "    inserting into map" << std::endl;
-    //     tile_map.insert({pt, theTiles[i]});
-    // }
 
 
+    // maps a point to an index of the TileImage array for retrive later
     map<Point<3>, size_t> tile_map;
     for (size_t i = 0; i < theTiles.size(); i++) {
         tile_point[i] = convertToXYZ(theTiles[i].getAverageColor());
@@ -113,54 +94,19 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
     }
 
 
-    std::cerr << "[mapTiles] Done preprocessing tiles. Building KDTree..." << std::endl;
-
     KDTree<3> tree(tile_point);
-    std::cerr << "[mapTiles] KDTree built. Starting second loop..." << std::endl;
 
-    // for (int i = 0; i < rows; i++) {
-    //     for (int j = 0; j < cols; j++) {
-    //         std::cerr << "  [source loop] (" << i << ", " << j << ")" << std::endl;
-
-    //         LUVAPixel source_pix = theSource.getRegionColor(i, j);
-    //         Point<3> query = convertToXYZ(source_pix);
-    //         Point<3> best_point = tree.findNearestNeighbor(query);
-
-
-    //         auto it = tile_map.find(best_point);
-    //         if (it == tile_map.end()) {
-    //             std::cerr << "    ERROR: tile_map missing best_point" << std::endl;
-    //             continue;
-    //         }
-
-    //         // std::cerr << " Setting tile img (" << i << ", " << j << ")" << std::endl;
-
-    //         // TileImage* tile_img = new TileImage(it->second);
-
-    //         // std::cerr << " After tile img (" << i << ", " << j << ")" << std::endl;
-
-    //         // store a pointer to the unique tile object instead of making a copy
-    //         TileImage* ref = &tile_map[best_point];
-    //         mosaic->setTile(i, j, ref);
-
-
-
-    //         mosaic->setTile(i, j, ref);
-    //     }
-    // }
-
-
+    // for each point in the sourceImg, identify the best match of image and set that tile to the mosaic canva
     for (int i = 0; i < theSource.getRows(); i++) {
-    for (int j = 0; j < theSource.getColumns(); j++) {
-        Point<3> query = convertToXYZ(theSource.getRegionColor(i, j));
-        Point<3> best_point = tree.findNearestNeighbor(query);
-        size_t index = tile_map[best_point];
-        mosaic->setTile(i, j, &theTiles[index]);  // use original
+        for (int j = 0; j < theSource.getColumns(); j++) {
+            Point<3> query = convertToXYZ(theSource.getRegionColor(i, j));
+            Point<3> best_point = tree.findNearestNeighbor(query);
+            size_t index = tile_map[best_point];
+            // &theTiles[index] gives a pointer to a tileImage
+            mosaic->setTile(i, j, &theTiles[index]);
+        }
     }
-}
 
-
-    std::cerr << "[mapTiles] Finished mapping tiles. Returning mosaic." << std::endl;
     return mosaic;
 }
 
